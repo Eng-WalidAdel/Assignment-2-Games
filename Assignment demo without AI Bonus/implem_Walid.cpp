104a60981290d597c0847674ef4fa2e8b06842d1
Walid Adel <khattabadel112005@gmail.com>
2024-12-03 17:23:31 +0200

    some

--- Assignment demo without AI Bonus/implem_Walid.cpp
+++ Assignment demo without AI Bonus/implem_Walid.cpp
@@ -1,3 +1,27 @@
-//
-// Created by A plus on 12/3/2024.
-//
+
+
+#include "BoardGame_Classes.h"
+
+template <typename T>
+class X_O_Board:public Board<T> {
+public:
+    X_O_Board ();
+    bool update_board (int x , int y , T symbol);
+    void display_board () ;
+    bool is_win() ;
+    bool is_draw();
+    bool game_is_over();
+
+};
+
+template <typename T>
+class X_O_Player : public Player<T> {
+public:
+    X_O_Player (string name, T symbol);
+    void getmove(int& x, int& y) ;
+
+};
+int main() {
+    return 0;
+}
+
--- implementation.cpp
+++ implementation.cpp
@@ -1,1 +1,1 @@
-#include "games.h"
+
