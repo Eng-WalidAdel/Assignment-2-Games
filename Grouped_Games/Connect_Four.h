e63d3b7468d123ef2cec2d665ef8e99fefaa2190
Eng-M0stafa Ehab <mostafaehabakl@gmail.com>
2024-12-16 15:00:42 +0200

    Readjusting.

--- Grouped_Games/Connect_Four.h
+++ Grouped_Games/Connect_Four.h
@@ -32,16 +32,7 @@ public:
 
     Four_in_a_row_player (string name, T symbol);
 
-//    explicit Four_in_a_row_player (T symbol): Player<T>(symbol){} ;
-
-//    string getname();
-//
-//    T getsymbol();
-
     void getmove(int& x , int& y);
-
-//    void setBoard(Board<T>* b);
-
 
 };
 
@@ -60,24 +51,6 @@ public:
 //================================ *** Implementation ***  =====================================================
 #include <bits/stdc++.h>
 using namespace std;
-//
-//#include <bits/stdc++.h>
-//#include "BoardGame_Classes.h"
-//#include "Connect_Four.h"
-//using namespace std;
-
-//template <typename T>
-//Four_in_a_row_Board<T>::Four_in_a_row_Board ()
-//{
-//    this->rows = 6;
-//    this->columns = 7;
-//    this->n_moves = 0;
-//
-//    this->board = make_unique <unique_ptr <T[]> [] > (this->rows);
-//    for (int i = 0; i < this->rows; ++i) {
-//        this->board[i] = make_unique <T[]> (this->columns);
-//    }
-//}
 
 
 template <typename T>
@@ -107,27 +80,6 @@ Four_in_a_row_Board<T>::~Four_in_a_row_Board() {
 
 
 
-
-//template <typename T>
-//Four_in_a_row_Board <T>::Four_in_a_row_Board()
-//{
-//    this->rows = 6;
-//    this->columns = 7;
-//
-//    this->board = new T*[this->rows];
-//    for (int i = 0; i < this->rows; ++i) {
-//        this->board[i] = new T[this->columns]{};
-//    }
-//}
-
-//template <typename T>
-//Four_in_a_row_Board<T> :: ~Four_in_a_row_Board() {
-//        for (int i = 0; i < this->rows; ++i) {
-//            delete[] this->board[i];
-//        }
-//        delete[] this->board;
-//    }
-
 template <typename T>
 void Four_in_a_row_Board<T>::display_board() {
     cout << "\n ";
@@ -145,19 +97,6 @@ void Four_in_a_row_Board<T>::display_board() {
         cout << "\n" << string(4 * this->columns + 1, '-') << endl;
     }
 }
-
-//template <typename T>
-//void Four_in_a_row_Board<T>::display_board() {
-//    for (int i = 0; i < this->rows; i++) {
-//        cout << "\n| ";
-//        for (int j = 0; j < this->columns; j++) {
-////            cout << "(" << i << "," << j << ")";
-//            cout << setw(2) << this->board[i][j] << " |";
-//        }
-//        cout << "\n-----------------------------";
-//    }
-//    cout << endl;
-//}
 
 
 template <typename T>
@@ -250,50 +189,6 @@ bool Four_in_a_row_Board<T>::game_is_over() {
 template <typename T>
 Four_in_a_row_player <T> :: Four_in_a_row_player (string name, T symbol) : Player<T> (name, symbol) {}
 
-//Four_in_a_row_player <T> :: Four_in_a_row_player (string n, T symbol) : Player<T>(n,symbol)
-//{
-//    this->name = n;
-//    this->symbol = symbol;
-//
-//}
-//
-//template <typename T>
-//Four_in_a_row_player <T> :: Four_in_a_row_player (T symbol) : Player<T>(symbol)
-//{
-//    this->name = "Computer";
-//    this->symbol = symbol;
-//}
-
-//    string setname(){
-//        string player_name;
-//        cout << "Please, enter the name you want to play with: ";
-//        cin >> player_name;
-//        return player_name;
-//    }
-//
-//    T setsymbol(){
-//        T player_symbol;
-//        cout << "Please, enter the symbol you want to play with: ";
-//        cin << player_symbol;
-//        return player_symbol;
-//    }
-
-//template <typename T>
-//string Four_in_a_row_player <T> :: getname()
-//{
-//    return this->name;
-//}
-//
-//template <typename T>
-//T Four_in_a_row_player <T> :: getsymbol()
-//{
-//    return this->symbol;
-//}
-
-//template <typename T>
-//void Four_in_a_row_player<T>::setBoard(Board<T>* b) {
-//    this->boardPtr = b;
-//}
 
 template <typename T>
 void Four_in_a_row_player <T> ::  getmove (int& x , int& y)
@@ -342,20 +237,7 @@ Four_in_a_row_random_player<T> :: Four_in_a_row_random_player (T symbol) : Rando
 
 template <typename T>
 void Four_in_a_row_random_player<T>::getmove(int& x, int& y) {
-//    do {
-//        y = rand() % this->dimension_y; // Random column
-//        if (this->boardPtr->board[0][y] == 0) {
-//            break; // Column is valid
-//        }
-//    } while (true);
-//
-//    // Find the lowest available row
-//    for (int i = this->rows - 1; i >= 0; --i) {
-//        if (this->boardPtr->board[i][y] == 0) {
-//            x = i;
-//            break;
-//        }
-//    }
+
     cout << this->name << "'s turn!" << endl;
     bool not_valid = true;
     while (not_valid)
@@ -372,21 +254,6 @@ void Four_in_a_row_random_player<T>::getmove(int& x, int& y) {
         }
     }
 }
-
-//template <typename T>
-//void Four_in_a_row_random_player<T>::getmove(int& x, int& y) {
-////    x = rand() % this->dimension_x;  // Random number between 0 and 5
-//    y = rand() % this->dimension_y;  // Random number between 0 and 6
-//
-//    for (int i = 1; i <= this -> boardPtr -> rows; ++i) {
-//
-//        if (this -> boardPtr -> board[i][y] != T() || i == (this -> boardPtr->rows) ){
-//            x = i - 1;
-//        }
-//    }
-//
-//}
-
 
 
 #endif //ASSIGNMENT_DEMO_CONNECT_FOUR_H
